d2dfc7386ba26c976bbb7fa41e0b44d2f330e716
vladsk2008fish-glitch <vlad.sk.2008.fish@gmail.com>
2026-08-30 20:33:29 +0200

    feat: add real OLX Lodz scraping script and dbt model

--- my_dbt_project/models/olx_houses.sql
+++ my_dbt_project/models/olx_houses.sql
@@ -1,6 +1,11 @@
 SELECT
-    id,
     title,
     price,
-    city
-FROM public.raw_olx_houses
+    location,
+    CASE
+        WHEN price < 2000 THEN 'Budget'
+        WHEN price BETWEEN 2000 AND 3500 THEN 'Medium'
+        ELSE 'Premium'
+    END AS price_category
+FROM {{ source('raw_data', 'raw_olx_houses') }}
+WHERE price IS NOT NULL
